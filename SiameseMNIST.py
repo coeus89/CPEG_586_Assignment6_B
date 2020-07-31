4dc2aacb25eea7975cd2b56fd745637016d13d14
Jonathan Karamavros <jkaramavros@live.com>
2020-07-30 21:49:35 -0400

    got the siamese working with classification from the handout. Moving On. Gotta come back and convert this later.

--- SiameseMNIST.py
+++ SiameseMNIST.py
@@ -12,41 +12,33 @@ def visualize(embed,labels):
     labelset = set(labels.tolist())
     fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(111)
+    #fig, ax = plt.subplots()
     for label in labelset:
         indices = np.where(labels == label)
-        ax.scatter(embed[indices,0],embed[indices,1],label=label,s=20)
+        ax.scatter(embed[indices,0], embed[indices,1], label = label, s = 20)
     ax.legend()
     #fig.savefig('embed.jpeg', format='jpeg', dpi=600, bbox_inches='tight')
     plt.show()
     plt.close()
 
 def main():
+    
+    #__________example 1 siamese_____________________
     # Load MNIST dataset
     mnist = input_data.read_data_sets('MNIST_data', one_hot = False)
     mnist_test_labels = mnist.test.labels
-    
-    # Load MNIST dataset
-    mnist = tf.keras.datasets.mnist
-    (x_test, y_test),(x_train,y_train) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-    x_train, x_test = tf.reshape(x_train,[10000,784]),tf.reshape(x_test,[60000,784])
-    # mnist = input_data.read_data_sets('MNIST_data', one_hot = False)
-    mnist = (x_train, y_train)
-    #mnist_test_labels = mnist.test.labels
-    mnist_test_labels = y_train
     siamese = Siamese()
-    siamese.trainSiamese(mnist,5000,100)
-    #test = keras.Sequential([
-    #    keras.layers.Conv2D(2,5,activation='relu',input_shape=(6,28,28,1))(x),
-    #    keras.layers.AveragePooling2D()
-    #    ])
+    siamese.trainSiamese(mnist,1000,128) # 5000, 128 produces good results
+    #siamese.saveModel()
+    #siamese.loadModel()
+    siamese.trainSiameseForClassification(mnist,1000,128)
 
     # Test model
-    embed = siamese.test_model(input = mnist.test.images)
-    # embed.tofile('embed.txt')
-    # embed = np.fromfile('embed.txt', dtype = np.float32)
+    embed = siamese.test_model(input1 = mnist.test.images)
     embed = embed.reshape([-1, 2])
     visualize(embed, mnist_test_labels)
+    siamese.computeAccuracy(mnist)
+
 
 
 
